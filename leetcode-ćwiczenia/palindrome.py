5596cafa08f0973fed965a32cae6e94bb1292ff0
IgnisTynka <marcia03835@gmail.com>
2022-10-16 21:47:26 +0200

    Palindrome

--- leetcode-ćwiczenia/palindrome.py
+++ leetcode-ćwiczenia/palindrome.py
@@ -1,22 +1,19 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
+def isPalindrome(x):
+    if x < 0:         # negative numbers are not palindromes
+        return False
 
-        if x < 0:         # negative numbers are not palindromes
-            return False
+    if x < 10:       # single digit numbers are palindromes
+        return True
 
-        if x < 10:       # single digit numbers are palindromes
-            return True
-        
-        if x % 10 == 0:      # numbers ending with 0 are not palindromes
-            return False
+    if x % 10 == 0:      # numbers ending with 0 are not palindromes
+        return False
 
-        rev = 0
-        while x > rev:      # reverse the number
-            rev = rev* 10 + x % 10
-            x = x / 10
-        
-        return x == rev or x == rev / 10
+    rev = 0
+    while x > rev:      # reverse the number
+        rev = rev* 10 + x % 10
+        x = x / 10
+    print(x)
+    print(rev)
+    return x == rev or x == rev / 10
+
+isPalindrome(121)
