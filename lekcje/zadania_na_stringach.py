bed02723001ba9d83489b57a65eb72404af9b474
IgnisTynka <marcia03835@gmail.com>
2022-11-10 15:26:20 +0100

    Zadania na stringach

--- lekcje/zadania_na_stringach.py
+++ lekcje/zadania_na_stringach.py
@@ -12,10 +12,19 @@ print("\nb)")
 for i in range(len(text)):
     print(text[(len(text)-1)-i], end=" ")
 
-# print("\nc)")
-# for i in range(len(text), ):
-#     if i+1 < len(text):
-#         print(text[i], text[i+1], end=" ")
-#     else:
-#         print(text[i], end=" ")
+print("\nc)")
+for i in range(0, len(text), 2):
+    if i+1 < len(text):
+        print(text[i], end="")
+        print(text[i+1], end=" ")
+    else:
+        print(text[i], end=" ")
+
+print("\nd)")
+for i in range(len(text)-1, 0, -2):
+    if i-1 >= 0:
+        print(text[i], end="")
+        print(text[i-1], end=" ")
+    else:
+        print(text[i], end=" ")
 
