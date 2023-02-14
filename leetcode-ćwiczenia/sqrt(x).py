cfce00769da72497edda67b497825126a3390aa6
IgnisTynka <marcia03835@gmail.com>
2023-02-14 08:50:28 +0100

    mySqrtI(x) and mySqrtF(x)

--- leetcode-ćwiczenia/sqrt(x).py
+++ leetcode-ćwiczenia/sqrt(x).py
@@ -1,4 +1,4 @@
-def mySqrt(x):
+def mySqrtI(x):
     if x == 1 or x == 0:
         return x
 
@@ -16,5 +16,28 @@ def mySqrt(x):
             end = mid - 1
     return result
 
+def mySqrtF(x):
+    if x < 2:
+        return x
+
+    start = 0
+    end = x // 2
+
+    while start <= end:
+        mid = (start + end) / 2
+        n = mid*mid
+        if ((n == x) or abs(n - x) < 0.00001):
+            return mid
+        elif n < x:
+            start = mid
+            result = mid
+        else:
+            end = mid
+
+    return result
+
 x = int(input("Podaj x:"))
-print(mySqrt(x))
+print(mySqrtI(x))
+
+x = float(input("Podaj x:"))
+print(mySqrtF(x))
